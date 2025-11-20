5c73e8df436b22895cf8b7ac878d637eebff9533
iherman7 <iherman@smith.edu>
2025-11-20 15:32:34 -0500

    neilson session

--- gameLoop.java
+++ gameLoop.java
@@ -1,14 +1,29 @@
 import java.util.Scanner;
 
 public class gameLoop{
-    
-        this.gameRunning = true;
-        while (this.gameRunning){
-            skeem skeem = new skeem();
-            Scanner main = new Scanner(System.in);
-            String userInput = main.nextLine();
-            // call all functions in the right orderrrr
-            // the last function will set gameRunning to false + the loop will be exited
+
+    public boolean gameRunning;
+    public String gameResponse;
+
+    public gameLoop(){
+
+    }
+
+    // method for start game dialogue
+
+    // method for reading user input 
+    public String readInput(){
+        Scanner main = new Scanner(System.in);
+        String userInput = main.nextLine(); 
+        return userInput;
+    }
+
+    public static void main(String[] args) {
+        skeem skeem = new skeem();
+        // start game
+        // read user input
+
+        
+
     }
 }
-
--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        gameLoop.gameLoop();
+        // gameLoop.gameLoop();
     }
   
 
