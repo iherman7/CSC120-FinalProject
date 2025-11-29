73e2840d6371610fece6fb1700978edba84aeea1
iherman7 <iherman@smith.edu>
2025-11-29 09:48:46 -0500

    testing userIO

--- main1.java
+++ main1.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class main1 {
 
     public static void main(String[] args) {
-        // gameLoop.gameLoop();
-        System.out.println(userIO.readInput());
+        int input;
+        String output;
+        userIO userIO = new userIO(1);
+        System.out.println("hi, start game");
+        input = userIO.readInput();
+        output = userIO.setOutput(input);
+        System.out.println(output);
+
     }
   
 
--- userIO.java
+++ userIO.java
@@ -2,23 +2,55 @@ import java.util.Scanner;
 
 public class userIO {
 
+    public int inputKey;
     public String output;
 
-    public userIO(String output){
-        this.output = output;
+    public userIO(int inputKey){
+        inputKey = 1;
     }
 
     // method for reading user input 
-    public String readInput(){
+    public int readInput(){
         Scanner mainScanner = new Scanner(System.in);
         String userInput = mainScanner.nextLine();
-            
-        if (userInput == "hi"){
-            this.output = "hello";
-        }
+        
+        String case1 = "hi";
+        String case2 = "bye";
+        String case3 = "help";
 
-        mainScanner.close();
-        return this.output;
+        if (userInput == case1){
+            return 1;
+        }
+        if (userInput == case2){
+            return 2;
+        }
+        if (userInput == case3){
+            return 3;
+        } else {
+            return 0;
+        }
+    }
+
+    public String setOutput(int inputKey){
+        if (inputKey == 1){
+            return "hello";
+        }
+        if (inputKey == 2){
+            return "goodbye";
+        }
+        if (inputKey == 3){
+            return "ok";
+        } else {
+            return "i don't understand";
+        }
+    }
+
+    public static void main(String[] args) {
+        userIO userIO = new userIO(1);
+        System.out.println("hi, start game");
+        int input = userIO.readInput();
+        String output = userIO.setOutput(input);
+        System.out.println(output);
     }
 
 }
