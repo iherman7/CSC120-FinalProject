b390b9564d3c2275390d91e36df6328a203e1868
johndoe <johndoe@email.com>
2025-12-07 22:50:02 -0500

    more gate stuff

--- gate.java
+++ gate.java
@@ -1,41 +1,47 @@
 public class gate extends location {
     
     private skeem player; 
+    public int change = 0;
 
     public gate(skeem player){
         this.player = player;
     }
         
 
-    public int oneZeroInput(){
+    public int oneZeroInput(hasKey){
+        while (true){
+            change = 0;
+            userIO userIO = new userIO();
+            String userInput = userIO.readInput();
 
-        userIO userIO = new userIO();
-        String userInput = userIO.readInput();
-
-        if (super.getX() == 1 && super.getY() == 0){
             String case1 = "WEST";
-            String case2 = "GO WEST"; 
+            String case2 = "GO WEST"; // check logic later, have to check first for case then key
+            String case3 = "NORTH";
+            String case4 = "GO NORTH";
+            String case5 = "SOUTH";
+            String case6 = "GO SOUTH";
 
-            if (player.hasKey() == false){ // need this condition; figure how to write it
+            if (userInput.equals(case1)|| userInput.equals(case2){ // will look differnt call from game loop
+                if (hasKey == false){ // need this condition; figure how to write it
+                    if (userInput.equals(case1)|| userInput.equals(case2)){
+                        change = 1;
+                        return change;
+                    }
+            }
+            if (hasKey == true){
                 if (userInput.equals(case1)|| userInput.equals(case2)){
-                    return 1;
-                }
-                else {
-                    return 0; 
+                    change = 2;
+                    return change;
                 }
             }
             else {
-                if (userInput.equals(case1)|| userInput.equals(case2)){
-                    return 2;
-                }
-                else {
-                    return 3; 
-                }
+            System.out.println("i don't understand");
             }
         }
-        else {
-            return 0;
         }
+
+        }
+
 
 
     }
@@ -45,7 +51,16 @@ public class gate extends location {
     //if boolean hasKey is true and they have said something along the lines of use key, open gate
     //then the rest of the game can continue
 
-public String oneZeroOutput(){
-
+public String oneZeroOutput(int inputKey){
+     if (inputKey == 1){
+            return " the gate is locked. i think you might need a key to open it ";
+        } 
+    if (inputKey == 2){
+            return " you use your cat mouth to put the key into the gate and it swings open! \n the gate was so tall the light coming in blinds you \n you wonder what is beyond the gate.";
+        } 
+    else {
+        return " i don't understand";
+    }
+    }
 }
 }
