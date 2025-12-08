45b6e4eb1ae28bdd363235e6b116c3e39a20672b
johndoe <johndoe@email.com>
2025-12-07 22:07:46 -0500

    gate stuff

--- gate.java
+++ gate.java
@@ -7,28 +7,42 @@ public class gate extends location {
     }
         
 
-    public int openingInput(){
+    public int oneZeroInput(){
 
         userIO userIO = new userIO();
         String userInput = userIO.readInput();
 
         if (super.getX() == 1 && super.getY() == 0){
-            if (player.hasKey() == false){ // need this condition; figure how to write it
+            String case1 = "WEST";
+            String case2 = "GO WEST"; 
 
-                String case1 = "WEST";
-                String case2 = "GO WEST";
+            if (player.hasKey() == false){ // need this condition; figure how to write it
                 if (userInput.equals(case1)|| userInput.equals(case2)){
                     return 1;
+                }
+                else {
+                    return 0; 
+                }
             }
-        }
+            else {
+                if (userInput.equals(case1)|| userInput.equals(case2)){
+                    return 2;
+                }
+                else {
+                    return 3; 
+                }
+            }
 
     }
 
+    }
 
     // set location
     //say you cannot do anything if boolean hasKey is false
     //if boolean hasKey is true and they have said something along the lines of use key, open gate
     //then the rest of the game can continue
-    
+
+public String oneZeroOutput(){
+
 }
 }
